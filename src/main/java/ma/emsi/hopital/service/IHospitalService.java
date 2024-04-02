98c0fccb038f2d10ab1c8f095ed4d40e51434a0e
LAMYA ELYADRHI <lamyaelyadrhi6@gmail.com>
2024-04-02 21:33:51 +0000

    First commit

---
+++ src/main/java/ma/emsi/hopital/service/IHospitalService.java
@@ -0,0 +1,17 @@
+package ma.emsi.hopital.service;
+
+import ma.emsi.hopital.entities.Consultation;
+import ma.emsi.hopital.entities.Medecin;
+import ma.emsi.hopital.entities.Patient;
+import ma.emsi.hopital.entities.RendezVous;
+
+public interface IHospitalService {
+    Patient savePatient(Patient patient);
+
+    Medecin saveMedecin(Medecin medecin);
+
+    RendezVous saveRDV(RendezVous rendezVous);
+
+    Consultation saveConsultation(Consultation consultation);
+
+}
